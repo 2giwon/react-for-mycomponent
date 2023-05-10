a3ef08f0d5aa0f733e6f9dd056bfae129804fb11
middlefgr <2giwon@gmail.com>
2023-05-10 22:28:36 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import ClassMyComponents from "./ClassMyComponents";
+import MyComponent from "./MyComponents";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyComponent name="React" favoriteNumber={1}>
+        리액트
+      </MyComponent>
+      <ClassMyComponents name="Class React" favoriteNumber={2}>
+        오오
+      </ClassMyComponents>
+    </div>
+  );
 }
 
 export default App;
